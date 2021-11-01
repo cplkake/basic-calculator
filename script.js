86d309191dfc97a9c8a2558bbd2e6e828e5c9ef8
cplkake <vinesh_pdas@yahoo.com>
2021-10-31 20:15:40 -0600

    created the user interface only without wiring up the JS

---
+++ script.js
@@ -0,0 +1,20 @@
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+function multiply (num1, num2) {
+return num1 * num2;
+}
+
+function subtract (num1, num2) {
+return num1 - num2;
+}
+
+function add (num1, num2) {
+return num1 + num2;
+}
+
+function operate(operation, num1, num2) {
+    return operation(num1, num2);
+}
+
